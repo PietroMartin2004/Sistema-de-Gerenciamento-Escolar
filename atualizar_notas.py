3d5025a7e94bd391a625bd16e74c355b63f13d61
Pietro Lima Martin <kyamasuki@gmail.com>
2024-09-19 14:40:45 -0300

    oi

--- atualizar_notas.py
+++ atualizar_notas.py
@@ -29,6 +29,7 @@ def notas():
 
         # Pede a unidade para alterar as notas
         option = int(input("Qual unidade deseja alterar a nota? (1° a 3°) "))
+        
         if option < 1 or option > 3:
             print("Opção inválida!")
             notas()
