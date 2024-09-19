333625dbcbb7392ed416fceea5b0f1c5c43e2d6e
Pietro Lima Martin <kyamasuki@gmail.com>
2024-09-19 14:59:24 -0300

    Verifica_login.py sem erro.

--- verificar_login.py
+++ verificar_login.py
@@ -1,29 +1,24 @@
 import conexao
 
 def verificar_credenciais():
-    global email,senha
+    global email, senha
     
     conexao.conectar_banco()
     
-    #Pergunta email e senha
-    
+    # Pergunta email e senha
     email = input("Qual o seu email? ")
     senha = input("Qual a sua senha? ")
     
-    #Conecta com o banco
-    
-    
-    #Faz a busca
-    
+    # Faz a busca
     resultado = conexao.conn.execute('''
     SELECT materia FROM professor WHERE email = ? AND senha = ?
     ''', (email, senha)).fetchone()
     
     if resultado:
-    #Printa o resultado da busca
-    	print(f"A sua matéria é: {resultado}")
-    	return email,senha
+        # Printa o resultado da busca
+        print(f"A sua matéria é: {resultado}")
+        return email, senha
     
     else:
-        print("Email e senhas inválidas")
+        print("Email e senha inválidos")
         verificar_credenciais()
